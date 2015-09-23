c822357a8ca95e56aacea81f5e99e510c9b7f4c8
Shawn Kohn <shawnkohn@gmail.com>
2015-09-23 19:25:19 -0400

    updated Debts with tests and some refactoring

--- app/controllers/debts_controller.rb
+++ app/controllers/debts_controller.rb
@@ -2,44 +2,47 @@ class DebtsController < ApplicationController
 	def new
 		@debt = Debt.new
 	end
+	
+    
+    def create
+		@debt = Debt.new(debt_params)
+		if @debt.save
+			redirect_to debts_path
+		else
+			render 'new'
+		end
+	end
 
-	def destroy
+	
+	def edit
+		@debt = Debt.find(params[:id])
+	end
+    
+    
+	def update
+  		@debt = Debt.find(params[:id])
+ 
+		  if @debt.update(debt_params)
+    			redirect_to debts_path
+  		  else
+    			render 'edit'
+  		  end
+	end
+    
+   
+    def destroy
   		@debt = Debt.find(params[:id])
   		@debt.destroy
  
 		  redirect_to debts_path
 	end
 	
-	def update
-  		@debt = Debt.find(params[:id])
- 
-		  if @debt.update(debt_params)
-    			redirect_to @debt
-  		  else
-    			render 'edit'
-  		  end
-	end
 
-	def edit
-		@debt = Debt.find(params[:id])
-	end
 
 	def index
 		@debts = Debt.all
 	end
 	
-	def create
-		@debt = Debt.new(debt_params)
-		if @debt.save
-			redirect_to @debt
-		else
-			render 'new'
-		end
-	end
-
-	def show
-		@debt = Debt.find(params[:id])
-	end
 
 	private
 		def debt_params
--- test/controllers/debts_controller_test.rb
+++ test/controllers/debts_controller_test.rb
@@ -1,19 +1,55 @@
 require "test_helper"
 
 class DebtsControllerTest < ActionController::TestCase
-        test "should get index" do
-                get :index
-                assert_response :success
-                assert_not_nil assigns(:debts)
+    
+    setup :initialize_debt
+    
+    
+    test "should get index" do
+        get :index
+        assert_response :success
+        assert_not_nil assigns(:debts)
+    end
+
+
+    test "should get new" do
+        get :new
+        assert_response :success
+    end
+    
+
+    test "should create debt" do
+        assert_difference('Debt.count') do
+            post :create, debt: {name: @debt.name, balance: @debt.balance, minimum_monthly_payment: @debt.minimum_monthly_payment, interest_rate: @debt.interest_rate}
         end
 
-        test "should create debt" do
-                assert_difference('Debt.count') do
-                        post :create, debt: {name: 'Test Debt', balance: 200, minimum_monthly_payment: 10, interest_rate: 0.22}
-                end
+        assert_redirected_to debts_path(assigns(:debts))
+    end
 
-                assert_redirected_to debt_path(assigns(:debt))
+    test "sould get edit" do
+        get :edit, id: @debt
+        assert_response :success
+    end
+
+    test "should update debt" do
+        @debt.name = "Updated Debt Name"
+        patch :update, id: @debt, debt: {name: @debt.name, balance: @debt.balance, minimum_monthly_payment: @debt.minimum_monthly_payment, interest_rate: @debt.interest_rate}
+        assert_redirected_to debts_path(assigns(:debts))
+    end
+
+
+    test "should destroy debt" do
+        assert_difference('Debt.count', -1) do
+            delete :destroy, id: @debt
         end
 
+        assert_redirected_to debts_path(assigns(:debts))
+        
+    end
+
+    private
+    def initialize_debt
+        @debt = debts(:one)
+    end
 
 end
