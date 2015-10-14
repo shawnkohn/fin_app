3dc39aef38caf6d86dda6a39e3722455da1a8a10
Shawn Kohn <shawnkohn@gmail.com>
2015-10-14 11:23:14 -0400

    still resolving migrate issues

---
+++ db/migrate/20150930010046_add_budget_id_to_debts.rb
@@ -0,0 +1,5 @@
+class AddBudgetIdToDebts < ActiveRecord::Migration
+      def change   
+          add_reference :debts, :budget, index: true, foreign_key: true
+      end
+end
---
+++ db/migrate/20151001193839_add_budget_id_to_step_one.rb
@@ -0,0 +1,5 @@
+class AddBudgetIdToStepOne < ActiveRecord::Migration
+      def change   
+          add_reference :step_ones, :budget, index: true, foreign_key: true
+      end
+end
